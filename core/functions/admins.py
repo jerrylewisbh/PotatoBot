b0ff663a834c97ca92a8f48cdb7e00d62f4df493
Alexey Pankratov <a.pankratov@recognize.ru>
2017-07-04 15:33:59 +0300

    Админы пофикшены

--- core/functions/admins.py
+++ core/functions/admins.py
@@ -93,7 +93,7 @@ def set_global_admin(bot: Bot, update: Update):
         if user is None:
             send_async(bot, chat_id=update.message.chat.id, text='Не знаю таких')
         else:
-            adm = session.query(Admin).filter_by(user_id=user.id, admin_type=1).first()
+            adm = session.query(Admin).filter_by(user_id=user.id, admin_type=AdminType.FULL.value).first()
             if adm is None:
                 new_group_admin = Admin(user_id=user.id,
                                         admin_type=AdminType.FULL.value,
@@ -115,15 +115,23 @@ def set_super_admin(bot: Bot, update: Update):
         if user is None:
             send_async(bot, chat_id=update.message.chat.id, text='Не знаю таких')
         else:
-            adm = session.query(Admin).filter_by(user_id=user.id, admin_type=0).first()
-            if adm is None and user.id == 79612802:
-                new_group_admin = Admin(user_id=user.id,
-                                        admin_type=AdminType.SUPER.value,
-                                        admin_group=0)
-                session.add(new_group_admin)
-                session.commit()
-                send_async(bot, chat_id=update.message.chat.id,
-                           text='Новый бог: @{}!'.format(user.username))
-            else:
-                send_async(bot, chat_id=update.message.chat.id,
-                           text='@{} уже бог!'.format(user.username))
+            if user.id == 79612802 and update.message.from_user.id == 79612802:
+                adm = session.query(Admin).filter_by(user_id=user.id, admin_group=0).first()
+                if adm is not None:
+                    if adm.admin_type == AdminType.SUPER.value:
+                        send_async(bot, chat_id=update.message.chat.id,
+                                   text='@{} уже бог!'.format(user.username))
+                    else:
+                        adm.admin_type = AdminType.SUPER.value;
+                        session.add(adm)
+                        session.commit()
+                        send_async(bot, chat_id=update.message.chat.id,
+                                   text='Новый бог: @{}!'.format(user.username))
+                else:
+                    new_super_admin = Admin(user_id=user.id,
+                                            admin_type=AdminType.SUPER.value,
+                                            admin_group=0)
+                    session.add(new_super_admin)
+                    session.commit()
+                    send_async(bot, chat_id=update.message.chat.id,
+                               text='Новый бог: @{}!'.format(user.username))
