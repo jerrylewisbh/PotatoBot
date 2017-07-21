50ac49df104787ec57b02dafab1180c93b29ba04
Alexey Pankratov <a.pankratov@recognize.ru>
2017-07-21 17:51:46 +0300

    Фикс профилей

--- core/template.py
+++ core/template.py
@@ -7,9 +7,9 @@ def fill_template(msg: str, user: User):
     if user.username:
         msg = msg.replace('%username%', '@' + user.username)
     else:
-        msg = msg.replace('%username%', user.first_name + ' ' + user.last_name)
-    msg = msg.replace('%first_name%', user.first_name)
-    msg = msg.replace('%last_name%', user.last_name)
+        msg = msg.replace('%username%', (user.first_name or '') + ' ' + (user.last_name or ''))
+    msg = msg.replace('%first_name%', user.first_name or '')
+    msg = msg.replace('%last_name%', user.last_name or '')
     msg = msg.replace('%id%', str(user.id))
     return msg
 
