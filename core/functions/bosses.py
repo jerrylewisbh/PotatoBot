cbf276a5e7c02bd122dd661757c1948ba2cec22d
Alexey Pankratov <a.pankratov@recognize.ru>
2017-08-08 14:27:41 +0300

    Фикс боссов

--- core/functions/bosses.py
+++ core/functions/bosses.py
@@ -12,7 +12,7 @@ def boss_leader(bot: Bot, update: Update):
     if len(group.squad) == 1:
         members = []
         for member in group.squad[0].members:
-            char = sorted(member.user[0].character, key=lambda x: x.date, reverse=True)[0]
+            char = sorted(session.query(User).filter_by(id=member.user_id).first().character, key=lambda x: x.date, reverse=True)[0]
             if 15 <= char.level <= 25:
                 members.append(repr(member.user[0]))
         msg = '\n'.join(members)
