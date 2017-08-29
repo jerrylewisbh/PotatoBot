c92fba66d3190d83c9f00912e18ea314d1a897ef
Alexey Pankratov <a.pankratov@recognize.ru>
2017-08-29 12:01:51 +0300

    Обновили деприкейтэд

--- core/functions/welcome.py
+++ core/functions/welcome.py
@@ -13,10 +13,10 @@ def welcome(bot: Bot, update: Update):
     newbie(bot, update)
     global last_welcome
     if update.message.chat.type in ['group', 'supergroup']:
-        if update.message.new_chat_member is not None:
+        group = update_group(update.message.chat)
+        for new_chat_member in update.message.new_chat_members:
             session = Session()
-            group = update_group(update.message.chat)
-            user = add_user(update.message.new_chat_member)
+            user = add_user(new_chat_member)
             administrator = session.query(Admin).filter_by(user_id=user.id).all()
             allow_anywhere = False
             for adm in administrator:
@@ -27,8 +27,8 @@ def welcome(bot: Bot, update: Update):
                     (user.member or user.member not in group.squad[0].members) and not allow_anywhere:
                 send_async(bot, chat_id=update.message.chat.id,
                            text=MSG_THORNS.format(str(user)))
-                bot.kickChatMember(update.message.chat.id, update.message.new_chat_member.id)
-                bot.unbanChatMember(update.message.chat.id, update.message.new_chat_member.id)
+                bot.kickChatMember(update.message.chat.id, new_chat_member.id)
+                bot.unbanChatMember(update.message.chat.id, new_chat_member.id)
             else:
                 if group.welcome_enabled:
                     welcome_msg = session.query(WelcomeMsg).filter_by(chat_id=group.id).first()
@@ -36,14 +36,14 @@ def welcome(bot: Bot, update: Update):
                         welcome_msg = WelcomeMsg(chat_id=group.id, message=MSG_WELCOME_DEFAULT)
                         session.add(welcome_msg)
 
-                    welcomed = session.query(Wellcomed).filter_by(user_id=update.message.new_chat_member.id,
+                    welcomed = session.query(Wellcomed).filter_by(user_id=new_chat_member.id,
                                                                   chat_id=update.message.chat.id).first()
                     if welcomed is None:
                         if time() - last_welcome > 30:
                             send_async(bot, chat_id=update.message.chat.id,
                                        text=fill_template(welcome_msg.message, user))
                             last_welcome = time()
-                        welcomed = Wellcomed(user_id=update.message.new_chat_member.id, chat_id=update.message.chat.id)
+                        welcomed = Wellcomed(user_id=new_chat_member.id, chat_id=update.message.chat.id)
                         session.add(welcomed)
                     session.commit()
 
