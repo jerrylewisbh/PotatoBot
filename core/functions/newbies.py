6b54e9d972b58f0ad8372257896358fa94e687d7
toxydose <a.yakovlev911@gmail.com>
2017-09-30 15:23:05 +0300

    newbies хардкоды и вкл/выкл функционала

--- core/functions/newbies.py
+++ core/functions/newbies.py
@@ -3,15 +3,16 @@ from core.types import User, Group, user_allowed
 from core.template import fill_template
 from core.utils import send_async
 from core.texts import *
+from config import ACADEM_CHAT_ID, CASTLE_CHAT_ID
 
-
-@user_allowed
-def newbie(bot: Bot, update: Update, session):
-    if update.message.chat.id in [-1001045426965]:
-        if update.message.new_chat_member is not None:
-            user = session.query(User).filter(User.id == update.message.new_chat_member.id).first()
-            if user is None:
-                group = session.query(Group).filter(Group.id == -1001146975451).first()
-                if group is not None:
-                    send_async(bot, chat_id=group.id,
-                               text=fill_template(MSG_NEWBIE, update.message.new_chat_member))
+if ACADEM_CHAT_ID and CASTLE_CHAT_ID:
+    @user_allowed
+    def newbie(bot: Bot, update: Update, session):
+        if update.message.chat.id in [CASTLE_CHAT_ID]:
+            if update.message.new_chat_member is not None:
+                user = session.query(User).filter(User.id == update.message.new_chat_member.id).first()
+                if user is None:
+                    group = session.query(Group).filter(Group.id == ACADEM_CHAT_ID).first()
+                    if group is not None:
+                        send_async(bot, chat_id=group.id,
+                                   text=fill_template(MSG_NEWBIE, update.message.new_chat_member))
