11e9314b6267614ecf859dc7c2e49796aa73c0f1
toxydose <a.yakovlev911@gmail.com>
2017-09-30 17:10:51 +0300

    //

--- core/functions/newbies.py
+++ core/functions/newbies.py
@@ -5,15 +5,14 @@ from core.utils import send_async
 from core.texts import *
 from config import ACADEM_CHAT_ID, CASTLE_CHAT_ID
 
-    @user_allowed
-
-    def newbie(bot: Bot, update: Update, session):
-        if ACADEM_CHAT_ID and CASTLE_CHAT_ID:
-        if update.message.chat.id in [CASTLE_CHAT_ID]:
-            if update.message.new_chat_member is not None:
-                user = session.query(User).filter(User.id == update.message.new_chat_member.id).first()
-                if user is None:
-                    group = session.query(Group).filter(Group.id == ACADEM_CHAT_ID).first()
-                    if group is not None:
-                        send_async(bot, chat_id=group.id,
-                                   text=fill_template(MSG_NEWBIE, update.message.new_chat_member))
+@user_allowed
+def newbie(bot: Bot, update: Update, session):
+    if ACADEM_CHAT_ID and CASTLE_CHAT_ID:
+    if update.message.chat.id in [CASTLE_CHAT_ID]:
+        if update.message.new_chat_member is not None:
+            user = session.query(User).filter(User.id == update.message.new_chat_member.id).first()
+            if user is None:
+                group = session.query(Group).filter(Group.id == ACADEM_CHAT_ID).first()
+                if group is not None:
+                    send_async(bot, chat_id=group.id,
+                               text=fill_template(MSG_NEWBIE, update.message.new_chat_member))
