a6c98d5fac6c97695caad07344a59e1b02189f9e
toxydose <a.yakovlev911@gmail.com>
2017-10-03 14:45:43 +0300

    перенос текстов в texts

--- core/functions/reply_markup.py
+++ core/functions/reply_markup.py
@@ -1,11 +1,11 @@
 from telegram import ReplyKeyboardMarkup, KeyboardButton
-
+from core.texts import *
 
 def generate_admin_markup(full=False, grp=False):
-    buttons = [[KeyboardButton('Приказы')]]
+    buttons = [[KeyboardButton(MSG_ORDER_BUTTON)]]
     if full:
-        buttons.append([KeyboardButton('Статус'), KeyboardButton('Группы')])
+        buttons.append([KeyboardButton(MSG_STATUS_BUTTON), KeyboardButton(MSG_GROUP_BUTTON)])
     if grp:
-        buttons.append([KeyboardButton('Заявки в отряд'), KeyboardButton('Чистка отряда')])
-    buttons.append([KeyboardButton('Список отряда')])
+        buttons.append([KeyboardButton(MSG_CLAIM_BUTTON), KeyboardButton(MSG_CLEAN_BUTTON)])
+    buttons.append([KeyboardButton(MSG_SQUAD_LIST_BUTTON)])
     return ReplyKeyboardMarkup(buttons, True)
