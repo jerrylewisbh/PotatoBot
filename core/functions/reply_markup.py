79f031de4739b1d426c2661aa3bee55b1949bacf
Alexey Pankratov <a.pankratov@recognize.ru>
2017-10-06 12:30:19 +0300

    Чуть переделана кнопка отряда и переход из админки в юзерпанель

--- core/functions/reply_markup.py
+++ core/functions/reply_markup.py
@@ -2,8 +2,8 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 from core.commands import ADMIN_COMMAND_ORDER, ADMIN_COMMAND_STATUS, ADMIN_COMMAND_GROUPS, ADMIN_COMMAND_RECRUIT, \
     ADMIN_COMMAND_FIRE_UP, ADMIN_COMMAND_SQUAD_LIST, USER_COMMAND_ME, USER_COMMAND_TOP, USER_COMMAND_SQUAD, \
-    USER_COMMAND_STATISTICS, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, ADMIN_COMMAND_ADMINPANEL,\
-    ADMIN_COMMAND_TO_USER_PANEL
+    USER_COMMAND_STATISTICS, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, ADMIN_COMMAND_ADMINPANEL, \
+    USER_COMMAND_SQUAD_REQUEST, USER_COMMAND_BACK
 
 
 def generate_admin_markup(full=False, grp=False):
@@ -13,14 +13,17 @@ def generate_admin_markup(full=False, grp=False):
     if grp:
         buttons.append([KeyboardButton(ADMIN_COMMAND_RECRUIT), KeyboardButton(ADMIN_COMMAND_FIRE_UP)])
     buttons.append([KeyboardButton(ADMIN_COMMAND_SQUAD_LIST)])
-    buttons.append([KeyboardButton(ADMIN_COMMAND_TO_USER_PANEL)])
+    buttons.append([KeyboardButton(USER_COMMAND_BACK)])
     return ReplyKeyboardMarkup(buttons, True)
 
 
-def generate_user_markup(is_admin=False):
-    buttons = [[KeyboardButton(USER_COMMAND_ME), KeyboardButton(USER_COMMAND_TOP)],
-               [KeyboardButton(USER_COMMAND_SQUAD), KeyboardButton(USER_COMMAND_STATISTICS)],
-               [KeyboardButton(USER_COMMAND_BUILD), KeyboardButton(USER_COMMAND_CONTACTS)]]
+def generate_user_markup(is_admin=False, in_squad=False):
+    buttons = [[KeyboardButton(USER_COMMAND_ME), KeyboardButton(USER_COMMAND_TOP)]]
+    if not in_squad:
+        buttons.append([KeyboardButton(USER_COMMAND_SQUAD_REQUEST), KeyboardButton(USER_COMMAND_STATISTICS)])
+    else:
+        buttons.append([KeyboardButton(USER_COMMAND_SQUAD), KeyboardButton(USER_COMMAND_STATISTICS)])
+    buttons.append([KeyboardButton(USER_COMMAND_BUILD), KeyboardButton(USER_COMMAND_CONTACTS)])
     if is_admin:
         buttons.append([KeyboardButton(ADMIN_COMMAND_ADMINPANEL)])
     return ReplyKeyboardMarkup(buttons, True)
--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from telegram.error import TelegramError
 
 from config import TOKEN, GOVERNMENT_CHAT
 from core.commands import ADMIN_COMMAND_STATUS, ADMIN_COMMAND_RECRUIT, ADMIN_COMMAND_ORDER, ADMIN_COMMAND_SQUAD_LIST, \
-    ADMIN_COMMAND_GROUPS, ADMIN_COMMAND_FIRE_UP, USER_COMMAND_ME, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS,\
-    USER_COMMAND_SQUAD, USER_COMMAND_STATISTICS, USER_COMMAND_TOP
+    ADMIN_COMMAND_GROUPS, ADMIN_COMMAND_FIRE_UP, USER_COMMAND_ME, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, \
+    USER_COMMAND_SQUAD, USER_COMMAND_STATISTICS, USER_COMMAND_TOP, USER_COMMAND_SQUAD_REQUEST, USER_COMMAND_BACK
 from core.functions.activity import (
     day_activity, week_activity, battle_activity
 )
@@ -188,7 +188,9 @@ def manage_all(bot: Bot, update: Update, session, chat_data, job_queue):
 
             if text == ADMIN_COMMAND_STATUS.lower():
                 send_status(bot, update)
-            elif text == 'хочу в отряд':
+            elif text == USER_COMMAND_BACK.lower():
+                user_panel(bot, update)
+            elif text == USER_COMMAND_SQUAD_REQUEST.lower():
                 squad_request(bot, update)
             elif text == ADMIN_COMMAND_RECRUIT.lower():
                 list_squad_requests(bot, update)
