bf3fade8fbb79fb4ea6765fc179b3d4d1c73bba1
Alexey Pankratov <a.pankratov@recognize.ru>
2017-10-06 12:03:50 +0300

    Переход из админки в юзерпанель

--- core/functions/reply_markup.py
+++ core/functions/reply_markup.py
@@ -2,7 +2,8 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 from core.commands import ADMIN_COMMAND_ORDER, ADMIN_COMMAND_STATUS, ADMIN_COMMAND_GROUPS, ADMIN_COMMAND_RECRUIT, \
     ADMIN_COMMAND_FIRE_UP, ADMIN_COMMAND_SQUAD_LIST, USER_COMMAND_ME, USER_COMMAND_TOP, USER_COMMAND_SQUAD, \
-    USER_COMMAND_STATISTICS, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, ADMIN_COMMAND_ADMINPANNEL
+    USER_COMMAND_STATISTICS, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, ADMIN_COMMAND_ADMINPANEL,\
+    ADMIN_COMMAND_TO_USER_PANEL
 
 
 def generate_admin_markup(full=False, grp=False):
@@ -12,6 +13,7 @@ def generate_admin_markup(full=False, grp=False):
     if grp:
         buttons.append([KeyboardButton(ADMIN_COMMAND_RECRUIT), KeyboardButton(ADMIN_COMMAND_FIRE_UP)])
     buttons.append([KeyboardButton(ADMIN_COMMAND_SQUAD_LIST)])
+    buttons.append([KeyboardButton(ADMIN_COMMAND_TO_USER_PANEL)])
     return ReplyKeyboardMarkup(buttons, True)
 
 
@@ -20,5 +22,5 @@ def generate_user_markup(is_admin=False):
                [KeyboardButton(USER_COMMAND_SQUAD), KeyboardButton(USER_COMMAND_STATISTICS)],
                [KeyboardButton(USER_COMMAND_BUILD), KeyboardButton(USER_COMMAND_CONTACTS)]]
     if is_admin:
-        buttons.append([KeyboardButton(ADMIN_COMMAND_ADMINPANNEL)])
+        buttons.append([KeyboardButton(ADMIN_COMMAND_ADMINPANEL)])
     return ReplyKeyboardMarkup(buttons, True)
