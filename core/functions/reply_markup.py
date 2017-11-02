a19ca1756e2b371686cbfdc3dc0f46a8b61524c1
Alexey Pankratov <a.pankratov@recognize.ru>
2017-11-02 17:02:56 +0300

    Показ репортов

--- core/functions/reply_markup.py
+++ core/functions/reply_markup.py
@@ -4,7 +4,7 @@ from core.commands import ADMIN_COMMAND_ORDER, ADMIN_COMMAND_STATUS, ADMIN_COMMA
     ADMIN_COMMAND_FIRE_UP, ADMIN_COMMAND_SQUAD_LIST, USER_COMMAND_ME, USER_COMMAND_TOP, USER_COMMAND_SQUAD, \
     USER_COMMAND_STATISTICS, USER_COMMAND_BUILD, USER_COMMAND_CONTACTS, ADMIN_COMMAND_ADMINPANEL, \
     USER_COMMAND_SQUAD_REQUEST, USER_COMMAND_BACK, TOP_COMMAND_ATTACK, TOP_COMMAND_DEFENCE, TOP_COMMAND_EXP, \
-    STATISTICS_COMMAND_EXP, USER_COMMAND_SQUAD_LEAVE
+    STATISTICS_COMMAND_EXP, USER_COMMAND_SQUAD_LEAVE, ADMIN_COMMAND_REPORTS
 
 
 def generate_admin_markup(full=False, grp=False):
@@ -43,7 +43,9 @@ def generate_statistics_markup():
     return ReplyKeyboardMarkup(buttons, True)
 
 
-def generate_squad_markup():
-    buttons = [[KeyboardButton(USER_COMMAND_SQUAD_LEAVE)],
-               [KeyboardButton(USER_COMMAND_BACK)]]
+def generate_squad_markup(is_group_admin=False):
+    buttons = [[KeyboardButton(USER_COMMAND_SQUAD_LEAVE)]]
+    if is_group_admin:
+        buttons.append([KeyboardButton(ADMIN_COMMAND_REPORTS)])
+    buttons.append([KeyboardButton(USER_COMMAND_BACK)])
     return ReplyKeyboardMarkup(buttons, True)
