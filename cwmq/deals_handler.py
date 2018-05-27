0a67a915469371468454afc1e827c8e37ed0d06f
Christopher Korn <mail@chkorn.de>
2018-05-27 17:54:41 +0200

    Generic error handler...

--- cwmq/deals_handler.py
+++ cwmq/deals_handler.py
@@ -15,25 +15,38 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 def deals_handler(channel, method, properties, body, dispatcher):
-    #logger.info('Received message # %s from %s: %s', method.delivery_tag, properties.app_id, body)
+    logger.info('Received message # %s from %s: %s', method.delivery_tag, properties.app_id, body)
     data = json.loads(body)
-    #print(data) # Debugging...
 
-    if data and "sellerId" in data:
-        user = session.query(User).filter_by(api_user_id=data['sellerId']).first()
+    try:
+        if data and "sellerId" in data:
+            user = session.query(User).filter_by(api_user_id=data['sellerId']).first()
 
-        if user and user.setting_automated_deal_report:
-            dispatcher.bot.send_message(
-                user.id,
-                MSG_DEAL_SOLD.format(data['item'],
-                                     (data['price'] * data['qty']),
-                                     data['qty'],
-                                     data['price'],
-                                     data['buyerCastle'],
-                                     data['buyerName']),
-                reply_markup=generate_user_markup(user.id),
-                parse_mode = ParseMode.HTML,
-            )
+            if user and user.setting_automated_deal_report:
+                dispatcher.bot.send_message(
+                    user.id,
+                    MSG_DEAL_SOLD.format(data['item'],
+                                         (data['price'] * data['qty']),
+                                         data['qty'],
+                                         data['price'],
+                                         data['buyerCastle'],
+                                         data['buyerName']),
+                    reply_markup=generate_user_markup(user.id),
+                    parse_mode = ParseMode.HTML,
+                )
 
-    # We're done...
-    channel.basic_ack(method.delivery_tag)
+        # We're done...
+        channel.basic_ack(method.delivery_tag)
+    except Exception as ex:
+        try:
+            # Acknowledge if possible...
+            channel.basic_ack(method.delivery_tag)
+        except Exception:
+            logging.exception("Can't acknowledge message")
+
+        try:
+            session.rollback()
+        except:
+            logging.exception("Can't do rollback")
+
+        logging.exception("Exception in MQ handler occured!")
