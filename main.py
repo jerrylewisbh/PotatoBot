fc69367348e0a55ee66d593e069579dd0fac7180
Christopher Korn <mail@chkorn.de>
2018-05-30 14:03:26 +0200

    Debugging for battle silence..

--- main.py
+++ main.py
@@ -112,8 +112,14 @@ def manage_all(bot: Bot, update: Update, session, chat_data, job_queue):
         admin = session.query(Admin).filter(
             Admin.user_id == update.message.from_user.id and
             Admin.admin_group in [update.message.chat.id, 0]).first()
+        
+        print("DEBUG SILENCE: State: {}, Squad: {}, Admin: {}".format(
+            get_game_state(),
+            squad.squad_name if squad else 'NO SQUAD', 
+            admin,
+        ))
 
-        if squad is not None and squad.silence_enabled and admin is None and get_game_state() == GameState.BATTLE_SILENCE:
+        if squad and squad.silence_enabled and not admin and get_game_state() == GameState.BATTLE_SILENCE:
             bot.delete_message(update.message.chat.id,
                                update.message.message_id)
         if not update.message.text:
