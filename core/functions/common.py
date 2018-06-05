29f864e3a8a71f6246f8513f09bb03436c84afdd
Christopher Korn <mail@chkorn.de>
2018-06-05 22:06:04 +0200

    Bugfix

--- core/functions/common.py
+++ core/functions/common.py
@@ -53,7 +53,7 @@ def user_panel(bot: Bot, update: Update):
                 welcome_text = MSG_START_MEMBER_SQUAD_REGISTERED.format(user.character.name)
 
                 if user.setting_automated_sniping and user.sniping_suspended:
-                    welcome_text + "\n\n" + SNIPE_SUSPENDED_NOTICE
+                    welcome_text += "\n\n" + SNIPE_SUSPENDED_NOTICE
             else:
                 welcome_text = MSG_START_MEMBER_SQUAD.format(user.character.name)
         else:
