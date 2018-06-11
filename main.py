9674f9e7554ad591bb712430aa3838a46dc1ac37
Christopher Korn <mail@chkorn.de>
2018-06-11 21:42:12 +0200

    We already use all Commans here. Why import them one by one...

--- main.py
+++ main.py
@@ -13,31 +13,8 @@ from telegram.ext.dispatcher import run_async
 
 from core.battle import report_after_battle
 from core.bot import MQBot
-from core.chat_commands import (CC_ADMIN_LIST, CC_ADMINS, CC_ALLOW_PIN_ALL,
-                                CC_ALLOW_TRIGGER_ALL, CC_BATTLE_STATISTICS,
-                                CC_CLOSE_HIRING, CC_DAY_STATISTICS, CC_DELETE,
-                                CC_DISALLOW_PIN_ALL, CC_DISALLOW_TRIGGER_ALL,
-                                CC_HELP, CC_KICK, CC_OPEN_HIRING, CC_PIN,
-                                CC_PING, CC_SET_TRIGGER, CC_SET_WELCOME,
-                                CC_SHOW_WELCOME, CC_SILENT_PIN, CC_SQUAD,
-                                CC_TRIGGER_LIST, CC_TURN_OFF_WELCOME,
-                                CC_TURN_ON_WELCOME, CC_UNSET_TRIGGER,
-                                CC_WEEK_STATISTICS)
-from core.commands import (ADMIN_COMMAND_ADMINPANEL, ADMIN_COMMAND_ATTENDANCE,
-                           ADMIN_COMMAND_FIRE_UP, ADMIN_COMMAND_GROUPS,
-                           ADMIN_COMMAND_ORDER, ADMIN_COMMAND_RECRUIT,
-                           ADMIN_COMMAND_REPORTS, ADMIN_COMMAND_SQUAD_LIST,
-                           ADMIN_COMMAND_STATUS, STATISTICS_COMMAND_EXP,
-                           STATISTICS_COMMAND_QUESTS,
-                           STATISTICS_COMMAND_SKILLS, TOP_COMMAND_ATTACK,
-                           TOP_COMMAND_BATTLES, TOP_COMMAND_BUILD,
-                           TOP_COMMAND_DEFENCE, TOP_COMMAND_EXP,
-                           USER_COMMAND_BACK, USER_COMMAND_EXCHANGE,
-                           USER_COMMAND_HIDE, USER_COMMAND_ME,
-                           USER_COMMAND_REGISTER, USER_COMMAND_SETTINGS,
-                           USER_COMMAND_SQUAD, USER_COMMAND_SQUAD_LEAVE,
-                           USER_COMMAND_SQUAD_REQUEST, USER_COMMAND_STATISTICS,
-                           USER_COMMAND_TOP)
+from core.chat_commands import *
+from core.commands import *
 from core.decorators import user_allowed
 from core.exchange.hide import hide_gold_info
 from core.exchange.snipe import sniping_info
