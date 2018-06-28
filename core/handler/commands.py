b3f6145e04c96e2ad973fbc8e001781c8e5997e6
Christopher Korn <mail@chkorn.de>
2018-06-28 22:09:19 +0200

    Split /items into two commands

--- core/handler/commands.py
+++ core/handler/commands.py
@@ -6,7 +6,7 @@ from functions.admins import (del_admin, del_global_admin, get_log,
                               list_admins, set_admin, set_global_admin,
                               set_super_admin)
 from functions.common import admin_panel, ban, help_msg, kick, ping, unban
-from functions.exchange import list_items
+from functions.exchange import list_items, list_items_other
 from functions.exchange.hide import auto_hide, hide_items, hide_list
 from functions.exchange.snipe import sniping, sniping_remove, sniping_resume
 from functions.guild.stock import deposit
@@ -90,6 +90,7 @@ def add_handler(disp: Dispatcher):
     disp.add_handler(CommandHandler('sr', sniping_remove, pass_args=True))
     disp.add_handler(CommandHandler('resume', sniping_resume))
     disp.add_handler(CommandHandler('items', list_items))
+    disp.add_handler(CommandHandler('items_other', list_items_other))
     disp.add_handler(CommandHandler('hide', hide_items))
     disp.add_handler(CommandHandler('hidelist', hide_list))
     disp.add_handler(CommandHandler('revoke', revoke))
--- functions/exchange/__init__.py
+++ functions/exchange/__init__.py
@@ -18,25 +18,45 @@ def get_item_by_cw_id(cw_id):
     return item
 
 
-@command_handler()
+@command_handler(
+    allow_group=True,
+    allow_private=True,
+)
 def list_items(bot: Bot, update: Update, user: User, **kwargs):
     args = None
     if "args" in kwargs:
         args = kwargs["args"]
 
     logging.info("list_items called by %s", update.message.chat.id)
-
-    text = ITEM_LIST
-
     items = Session.query(Item).filter(Item.tradable == True).order_by(Item.cw_id).all()
-    text += "Tradable items:\n"
+    text = "*Tradable items:*\n"
     for item in items:
         text += "`{}` {}\n".format(pad_string(item.cw_id, 4), item.name)
+    text += "\nFor a list of additional items that can only be traded in via Auction see /items\_other"
+
+    send_async(
+        bot,
+        chat_id=update.message.chat.id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
+@command_handler(
+    allow_group=True,
+    allow_private=True,
+)
+def list_items_other(bot: Bot, update: Update, user: User, **kwargs):
+    args = None
+    if "args" in kwargs:
+        args = kwargs["args"]
+
+    logging.info("list_items called by %s", update.message.chat.id)
 
     items = Session.query(Item).filter(Item.tradable == False).order_by(Item.cw_id).all()
-    text += "\nNot tradable via Exchange or new items:\n"
+    text = "*Items not tradable via Exchange or new items:*\n"
     for item in items:
         text += "`{}` {}\n".format(pad_string(item.cw_id, 4), item.name)
+    text += "\nFor a list of additional items that can be traded in the Exchange see /items"
 
     send_async(
         bot,
