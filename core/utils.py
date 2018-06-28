42803f621133a100a787e15796517d033c99fbcf
Christopher Korn <mail@chkorn.de>
2018-06-28 20:09:48 +0200

    remove send_async error handler, this should be handled by generic handlers

--- core/utils.py
+++ core/utils.py
@@ -3,25 +3,15 @@ from telegram import Bot
 from telegram.error import TelegramError
 from telegram.ext.dispatcher import run_async
 
+from core.bot import MQBot
 from core.types import Group, Session, User
 
 Session()
 
 
 @run_async
-def send_async(bot: Bot, *args, **kwargs):
-    try:
-        return bot.sendMessage(*args, **kwargs)
-
-    except TelegramError as err:
-        bot.logger.error(err.message)
-        group = Session.query(Group).filter_by(id=kwargs['chat_id']).first()
-        if group is not None:
-            group.bot_in_group = False
-            Session.add(group)
-            Session.commit()
-        return None
-
+def send_async(bot: MQBot, *args, **kwargs):
+    return bot.send_message(*args, **kwargs)
 
 def create_or_update_user(telegram_user: telegram.User) -> User:
     """
