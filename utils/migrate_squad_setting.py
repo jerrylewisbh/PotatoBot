474fec2efab47d67db3f4148873634cd8968d78c
Christopher Korn <mail@chkorn.de>
2018-07-15 23:35:21 +0200

    Path-Fix

--- utils/migrate_squad_setting.py
+++ utils/migrate_squad_setting.py
@@ -1,29 +1,21 @@
-import codecs
-import csv
 import os
 import sys
-import urllib.request
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from sqlalchemy import text
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../botato/')))
 
 # noinspection PyPep8
 from core.db import Session
-from core.model import Item
+from core.model import Group
 
 Session()
 
-the_url = input("Url to Heroku Dataclip CSV: ")
-
-with urllib.request.urlopen(the_url) as url:
-    the_file = codecs.iterdecode(url, 'utf-8')
-    for line in csv.DictReader(the_file):
-        item = Session.query(Item).filter(
-            Item.name == line['name'],
-            Item.cw_id.is_(None)
-        ).first()
-
-        if item:
-            print("Found missing cw_id='{}' for name='{}'".format(line['id'], line['name']))
-            item.cw_id = line['id']
-            Session.add(item)
-            Session.commit()
+result = Session.execute(text('SELECT chat_id, reminders_enabled, thorns_enabled, silence_enabled FROM squads'))
+for squad in result:
+    group = Session.query(Group).filter(Group.id == squad[0]).first()
+    group.reminders_enabled = squad[1]
+    group.thorns_enabled = squad[2]
+    group.silence_enabled = squad[3]
+    Session.add(group)
+    Session.commit()
