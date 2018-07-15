bbb6e47bc23a2ba15161a751476408a5911eeea0
Christopher Korn <mail@chkorn.de>
2018-07-15 08:44:24 +0200

    Start with Alembic

---
+++ alembic/versions/9497338259de_initial.py
@@ -0,0 +1,24 @@
+"""Initial
+
+Revision ID: 9497338259de
+Revises: 
+Create Date: 2018-07-15 06:43:47.899194+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9497338259de'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
