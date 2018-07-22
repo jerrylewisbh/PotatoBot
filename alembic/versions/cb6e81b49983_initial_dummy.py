5ee146f836bdf4b37b6a30307928f84345c54ac2
Christopher Korn <mail@chkorn.de>
2018-07-22 23:55:40 +0200

    Cleanup

---
+++ alembic/versions/cb6e81b49983_initial_dummy.py
@@ -0,0 +1,14 @@
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+revision = 'cb6e81b49983'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
