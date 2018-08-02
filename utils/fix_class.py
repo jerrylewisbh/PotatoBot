3116163aa174b53cd066d786e87dcbac5e4578ea
Christopher Korn <mail@chkorn.de>
2018-08-02 22:28:49 +0200

    Speed up Fixing...

--- utils/fix_class.py
+++ utils/fix_class.py
@@ -3,6 +3,8 @@ import csv
 import os
 import sys
 
+from sqlalchemy import update
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../botato')))
 
 # noinspection PyPep8
@@ -12,20 +14,23 @@ from core.model import Item, Character, Profession
 Session()
 
 
-c = Session.query(Character).all()
+c = Session.query(Character.user_id).distinct().all()
 
-for char in c:
-    user_profession = Session.query(Profession).filter(
-        Profession.user_id == char.user_id
-    ).order_by(Profession.date.desc()).first()
+mapping = {
+    'Master': 'Esquire / Master',
+    'Esquire': 'Esquire / Master',
+}
+
+for user_id in c:
+    user_id = user_id[0]
+    user_profession = Session.query(Profession).filter(Profession.user_id == user_id).order_by(Profession.date.desc()).first()
 
     if not user_profession:
         continue
 
-    if char.characterClass != user_profession.name:
-        print("Setting {} for {}".format(user_profession.name, char.name))
-        char.characterClass = user_profession.name
-        Session.add(char)
+    if user_profession.name:
+        print("Setting {} for {}".format(user_profession.name, user_id))
+        Session.query(Character).filter(Character.user_id == user_id).update({"characterClass": mapping.get(user_profession.name, user_profession.name)})
         Session.commit()
 
 
